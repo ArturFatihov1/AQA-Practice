104fe694f993c40a08cf061bd449c6f6c4dff688
Artur <artur.fatihov23@mail.ru>
2026-05-19 15:11:09 +0500

    ci cd2

--- src/test/java/core/BaseTest.java
+++ src/test/java/core/BaseTest.java
@@ -3,7 +3,6 @@ package core;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,8 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 public abstract class BaseTest {
 
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        Configuration.remote = "http://selenoid:4444/wd/hub";
+        Configuration.remote = "http://localhost:4444/wd/hub";
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.headless = true;
